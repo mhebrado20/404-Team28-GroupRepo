509ca29009610451b12cc7bc5cec4abdf1f063fd
Scott Kevil-Yeager <scottky00@tamu.edu>
2022-09-12 10:22:02 -0500

    changing directory names and stuff

---
+++ Scott/ECEN404Project/i2s_sampling/.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
+ C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
