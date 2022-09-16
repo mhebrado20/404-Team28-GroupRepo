1ea59a059db522b898f698083dc4f9885972dd92
Scott Kevil-Yeager <scottky00@tamu.edu>
2022-09-15 19:21:44 -0500

    code updates

--- Scott/ECEN404Project/NCID_Database/services/JSON_data_transfer.py
+++ Scott/ECEN404Project/NCID_Database/services/JSON_data_transfer.py
@@ -1,15 +1,18 @@
 import json
 import serial
+import wave
 
 connection = serial.Serial(port="COM3", baudrate=115200)
 connection.reset_input_buffer()
 
-while True:
-    data = connection.readline()
-    print(data)
+# open the file, name it and set it to write
+audiofile = wave.open('C:/Users/sky20/Desktop/serialrecording/sound.wav', 'wb')
+audiofile.setframerate(16000)  # sample rate
+audiofile.setnchannels(1)  # mono(1) or stereo(2)
+audiofile.setsampwidth(2)
 
-    #try:
-    #    dict_json = json.loads(data)
-    #    print(dict_json)
-    #except json.JSONDecodeError as e:
-    #    print("JSON:", e)
+while connection.isOpen():
+    data = connection.readline()  # read the command line data
+    audiofile.writeframes(data)
+
+audiofile.close()
--- Scott/ECEN404Project/i2s_sampling/src/main.cpp
+++ Scott/ECEN404Project/i2s_sampling/src/main.cpp
@@ -9,14 +9,14 @@
 #include <iostream>
 #include <cstdio>
 
-//WiFiClient *wifiClientADC = NULL;
-//HTTPClient *httpClientADC = NULL;
-//WiFiClient *wifiClientI2S = NULL;
-//HTTPClient *httpClientI2S = NULL;
+// WiFiClient *wifiClientADC = NULL;
+// HTTPClient *httpClientADC = NULL;
+// WiFiClient *wifiClientI2S = NULL;
+// HTTPClient *httpClientI2S = NULL;
 ADCSampler *adcSampler = NULL;
 I2SSampler *i2sSampler = NULL;
 
-//DynamicJsonDocument recording(2048);
+// DynamicJsonDocument recording(2048);
 
 // replace this with your machines IP Address
 //#define ADC_SERVER_URL "file:///C:/Users/sky20/Desktop/serialrecording"
@@ -31,7 +31,7 @@ i2s_config_t adcI2SConfig = {
     .communication_format = I2S_COMM_FORMAT_I2S_LSB,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 4,
-    .dma_buf_len = 1024, //changed from 1024
+    .dma_buf_len = 1024,
     .use_apll = false,
     .tx_desc_auto_clear = false,
     .fixed_mclk = 0};
@@ -53,11 +53,11 @@ i2s_config_t i2sMemsConfigLeftChannel = {
 */
 
 // i2s pins
-i2s_pin_config_t i2sPins = { //this type configures the pins of the microcontroller board that you have chosen to use to convert from analog to digital signals
+i2s_pin_config_t i2sPins = { // this type configures the pins of the microcontroller board that you have chosen to use to convert from analog to digital signals
     .bck_io_num = GPIO_NUM_32,
     .ws_io_num = GPIO_NUM_25,
     .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = GPIO_NUM_35}; //change this pin to match the output pin of whatever microcontroller board you have chosen to use
+    .data_in_num = GPIO_NUM_33};
 
 // how many samples to read at once
 const int SAMPLE_SIZE = 16384;
@@ -66,12 +66,12 @@ const int SAMPLE_SIZE = 16384;
 void sendData(uint8_t *bytes, size_t count)
 {
   // send them off to the serial port
-  //digitalWrite(2, HIGH);
-  //httpClient->begin(url);
-  //httpClient->addHeader("content-type", "application/octet-stream");
-  //httpClient->POST(bytes, count);
-  //httpClient->end();
-  //digitalWrite(2, LOW);
+  // digitalWrite(2, HIGH);
+  // httpClient->begin(url);
+  // httpClient->addHeader("content-type", "application/octet-stream");
+  // httpClient->POST(bytes, count);
+  // httpClient->end();
+  // digitalWrite(2, LOW);
   DynamicJsonDocument recording(2048);
   recording["audioData"] = bytes;
   serializeJson(recording, Serial);
@@ -83,7 +83,7 @@ void sendData(uint8_t *bytes, size_t count)
 // Task to write samples from ADC to our server
 void adcWriterTask(void *param)
 {
-  I2SSampler *sampler = (I2SSampler *)param; //changed I2SSampler to ADCSampler
+  I2SSampler *sampler = (I2SSampler *)param; // changed I2SSampler to ADCSampler
   int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
   if (!samples)
   {
@@ -94,18 +94,17 @@ void adcWriterTask(void *param)
   while (true)
   {
     int samples_read = sampler->read(samples, SAMPLE_SIZE);
-    //sendData(wifiClientADC, httpClientADC, ADC_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
+    // sendData(wifiClientADC, httpClientADC, ADC_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
     sendData((uint8_t *)samples, samples_read * sizeof(uint16_t));
   }
 }
-
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Started up");
 
-  //recording["audioData"] = "";
+  // recording["audioData"] = "";
 
   // launch WiFi
   /*
@@ -117,7 +116,7 @@ void setup()
     Serial.print(".");
     delay(1000);
   }
-  
+
   Serial.println("");
   Serial.println("WiFi Connected");
   Serial.println("Started up");
@@ -140,7 +139,6 @@ void setup()
   TaskHandle_t adcWriterTaskHandle;
   xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1, &adcWriterTaskHandle, 1);
   adcSampler->start();
-
 }
 
 void loop()
