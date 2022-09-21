881137644279a9f402ac66349144611f309cb296
Scott Kevil-Yeager <scottky00@tamu.edu>
2022-09-21 14:25:36 -0500

    Update JSON_data_transfer.py

--- Scott/ECEN404Project/NCID_Database/services/JSON_data_transfer.py
+++ Scott/ECEN404Project/NCID_Database/services/JSON_data_transfer.py
@@ -2,32 +2,68 @@ import json
 import serial
 import wave
 import io
+import time
+import pyaudio
 import numpy as np
-import soundfile as sf
-from scipy.io.wavfile import write
+import scipy.io.wavfile
 
 connection = serial.Serial(port="COM3", baudrate=115200)
 connection.reset_input_buffer()
 
-# open the file, name it and set it to write
-audiofile = wave.open('C:/Users/sky20/Desktop/serialrecording/sound.wav', 'wb')
-audiofile.setframerate(16000)  # sample rate
-audiofile.setnchannels(1)  # mono(1) or stereo(2)
-audiofile.setsampwidth(2)
+incoming_serial = connection.readline()
+chunk = 1024
 
+timeout = time.time() + (60 * .25)
 
-def convert_bytearray_to_wav_ndarray(input_bytearray: bytes, sampling_rate=16000):
-    bytes_wav = bytes()
-    byte_io = io.BytesIO(bytes_wav)
-    write(byte_io, sampling_rate, np.frombuffer(input_bytearray, dtype=np.uint8))
-    output_wav = byte_io.read()
-    output, samplerate = sf.read(io.BytesIO(output_wav))
-    return output
+# wf = wave.open('C:/Users/sky20/Desktop/serialrecording/sound2.wav', 'wb')
+# wf.setnchannels(1)
+# wf.setsampwidth(2)
+# wf.setframerate(16000)
 
+# while time.time() < timeout:
+#     d = wf.writeframes(incoming_serial)
 
-while connection.isOpen():
-    data = connection.readline()  # read the command line data
-    audiofile = convert_bytearray_to_wav_ndarray(data)
+# wf.close()
 
-scipy.io.wavfile.write('C:/Users/sky20/Desktop/serialrecording/sound.wav', 16000, audiofile)
-audiofile.close()
+# 8 bits per sample
+sample_format = pyaudio.paUInt8
+chanels = 1
+
+# Record at 16000 samples per second
+smpl_rt = 16000
+seconds = 4
+filename = 'C:/Users/sky20/Desktop/serialrecording/sound3.wav'
+
+# Create an interface to PortAudio
+pa = pyaudio.PyAudio()
+
+stream = pa.open(format=sample_format, channels=chanels,
+                 rate=smpl_rt, input=True,
+                 frames_per_buffer=chunk)
+
+print('Recording...')
+
+# Initialize array that be used for storing frames
+frames = []
+
+# Store data in chunks for 8 seconds
+for i in range(0, int(smpl_rt / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream
+stream.stop_stream()
+stream.close()
+
+# Terminate - PortAudio interface
+pa.terminate()
+
+print('Done !!! ')
+
+# Save the recorded data in a .wav format
+sf = wave.open(filename, 'wb')
+sf.setnchannels(chanels)
+sf.setsampwidth(pa.get_sample_size(sample_format))
+sf.setframerate(smpl_rt)
+sf.writeframes(b''.join(frames))
+sf.close()
