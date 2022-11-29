dcb7c71a125d22bc8475a21f82995687f5e9e69b
jiayichen513 <jychen@tamu.edu>
2022-11-29 12:50:53 -0600

    Deleted old code

--- Amy/ESP8266CallerID/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.o: \
- C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
- C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/sky20/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
